852482fbc8883a7ce30f916dfc44b6108b506ff4
megosparta <megosparta@mail.ru>
2015-02-10 01:09:46 +0300

    added some files

--- grep.rb
+++ grep.rb
@@ -1,16 +1,18 @@
 #!/usr/bin/env ruby
 
 class Grep 
-  def initialize(str = "class", file_name = File.dirname(__FILE__) + "/grep.rb")
-    @str = str
-    @file_name = File.dirname(__FILE__) + "#{file_name}"
+  def initialize
+
   end
 
-  def search
-    file_content = File.open(@file_name){ |file| file.read.split("\n") }
-    file_content.each { |line| puts line if line.include? "#{@str}" }  
+  def search(patern, file_names)
+    file_names.each do |file|
+      file_content = File.open(file){ |line| line.read.split("\n") }
+      file_content.each { |line| puts line if line =~ /#{patern}/ }  
+    end
   end
 end
 
-a = Grep.new("adfasdf","/grep.rb")
-a.search
+file_names = ["grep.rb", "Gemfile"] 
+a = Grep.new
+a.search("rails", file_names)

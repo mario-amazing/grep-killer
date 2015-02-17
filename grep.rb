1e879f3e422dd13ccf44c6f80fb25f6746f3fa60
megosparta <megosparta@mail.ru>
2015-02-17 22:22:26 +0300

    add Recognition options

--- grep.rb
+++ grep.rb
@@ -1,16 +1,42 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
+USAGE = "Usage: search string + function + [option]"
+
 class Grep 
   def initialize
+
   end
 
-  def search(patern, file_names)
-    file_names.each do |file|
-    File.open(file).each { |line| puts line if line =~ /#{patern}/ }  
+  def search(options) 
+    options[:fnames].each do |file|
+    File.open(file).each { |line| puts line if line =~ /#{options[:patern]}/ }  
     end
   end
 end
 
-file_names = ["grep.rb", "Gemfile"] 
+def parse_argv(args)
+  options = {}
+  options[:context] = 0
+  options[:patern] = args[0]
+  options[:fnames] = args[1].split(",")
+  OptionParser.new do |opts|
+    opts.banner = USAGE
+    opts.on("-A","puts string with context") do
+      options[:context] = 1
+    end
+    opts.on("-h","--help","Options info") do
+      puts opts
+      exit
+    end
+  end.parse!(args)
+  options
+end   
+
+#file_names = ["grep.rb", "Gemfile"] 
+options = parse_argv(ARGV)
 a = Grep.new
-a.search("rails", file_names)
+a.search(options)
+
+#puts ARGV
+#a.search("rails", file_names, "-h")

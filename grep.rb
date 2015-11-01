9be706a26e5a6da00e2d70fdad5842fba584e8bb
megosparta <megosparta@mail.ru>
2015-11-01 15:28:12 +0300

    Added Gzip parse and searsh option

--- grep.rb
+++ grep.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
 require 'optparse'
-require 'zip'
 require 'pry'
 require 'colorize'
+require 'zlib'
 
 class Args
   def initialize(args)
@@ -11,33 +11,32 @@ class Args
   end
 
   def parse_argv
-    @conditions = { amount: 0, fnames: [] }
+    @conditions = { amount: 0 }
     OptionParser.new do |opts|
-      opts.banner = 'Usage: [options] + PATTERN + [FILE]'
-      opts.on('-A NLINES', Integer, 'Puts string with amount') do |amount|
+      opts.banner = 'Usage: PATTERN + FILE + [options]'
+      opts.on('-A NLINES', Integer, 'Amount of context') do |amount|
         @conditions[:amount] = amount
       end
       opts.on('-f', '-files fn1,fn2,fn3', Array, 'Files for search') do |files|
-        @conditions[:fnames] << files
+        @conditions[:fnames] = files
       end
       opts.on('-R', 'Recursion in the current directory') do |_|
-        @conditions[:fnames] << Dir.glob('*')
+        @conditions[:fnames] = Dir.glob('*')
       end
-      opts.on('-z znames', Array, 'In what zip file need to search') do |zname|
-        @conditions[:zip] = znames
+      opts.on('-z zname', String, 'Gzip file for search') do |zname|
+        @conditions[:zname] = zname
       end
     end.parse!(@args)
-    # binding.pry
     @conditions[:pattern] = @args.shift
-    @conditions[:fnames] << @args.shift if !@args.first.nil?
+    @conditions[:fnames] << @args.shift unless @args.first.nil?
     validate_coditions
     @conditions
   end
 
   def validate_coditions
-      binding.pry
-      if @conditions[:pattern].nil? || (@conditions[:fnames].none? && @conditions[:zname].nil?)
-      raise 'Wrong conditions format.'.red
+    if @conditions[:pattern].nil? ||
+       (@conditions[:fnames].nil? && @conditions[:zname].nil?)
+      fail 'Wrong conditions format.Use -h for help.'.red
     end
   end
 end
@@ -48,28 +47,33 @@ class Grep
   end
 
   def search_pattern
-    unzip_file(@conditions[:zip]) if !@conditions[:zip].nil?
-    find_content = []
-    @conditions[:fnames].each do |fname|
-      content = open_file(fname)
-      scope = verification_pattern(content)
-      # binding.pry
-      find_content << { fname: fname, content: scope } if !scope.empty?
+    @find_content = []
+    unless @conditions[:zname].nil?
+      content = unzname_file(@conditions[:zname])
+      verification_pattern(content, @conditions[:zname])
     end
-    find_content
+    unless @conditions[:fnames].nil?
+      @conditions[:fnames].each do |fname|
+        content = open_file(fname)
+        verification_pattern(content, fname)
+      end
+    end
+    @find_content
   end
 
-  def verification_pattern(content, pattern = @conditions[:pattern])
+  def verification_pattern(content, fname, pattern = @conditions[:pattern])
     amount = @conditions[:amount]
     scope = []
-    if !content.nil?
+    unless content.nil?
       content.each_with_index do |line, index|
         if line =~ /#{pattern}/
           scope << (content[index - amount..index + amount].join).green
         end
       end
     end
-    scope
+    @find_content << { fname: fname, content: scope } unless scope.empty?
+  rescue
+    puts "File #{fname} have unreadable format\n".red
   end
 
   def open_file(fname)
@@ -77,28 +81,25 @@ class Grep
     File.open(fname).each { |line| content << line }
     content
   rescue
-    puts "File: #{fname} cant be open.".red
+    puts "File: #{fname} cant be open.\n".red
   end
 
   def to_s
+    search_pattern
     find_content = ''
-    search_pattern.each do |parse|
+    @find_content.each do |parse|
       find_content << (parse[:fname] + ":\n").blue
       parse[:content].each { |content| find_content << content }
     end
     find_content
   end
 
-  def unzip_file
-    Zip::File.open(@conditions[:zip]) do |zip_file|
-      Dir.mkdir('tmp_dir') unless Dir.exist?('tmp_dir')
-      zip_file.each do |files|
-        zip_files.extract(files, "#{Dir.pwd}/tmp_dir") unless
-        File.exist?('#{Dir.pwd}/tmp_dir')
-        @conditions[:fnames] = Dir.glob('tmp_dir/*')
-      end
-      Dir.delete('./tmp_dir')
-    end
+  def unzname_file(zname)
+    content = []
+    Zlib::GzipReader.open(zname).each { |line| content << line }
+    content
+  rescue
+    puts "GZip file: #{zname} cant be open\n".red
   end
 end
 
